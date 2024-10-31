e62d8ea6a22e8efb9ad83c62669d0e2b16bdcb8d
Ivan Shcherbinin <ivan.shcherbinin1991@gmail.com>
2024-10-31 16:14:21 +0400

    Add more validation for values

--- validators.py
+++ validators.py
@@ -7,12 +7,18 @@ async def validate_date(date: str) -> Optional[datetime]:
         chosen_date = datetime.strptime(date, "%d.%m")
     except ValueError:
         return None
+    today = datetime.now().date()
+    if chosen_date.date() < today:
+        return None
     return chosen_date
 
 async def validate_time(time: str) -> Optional[datetime]:
     try:
         chosen_time = datetime.strptime(time, "%H:%M")
     except ValueError:
+        return None
+    now = datetime.now()
+    if chosen_time.time() < now.time():
         return None
     return chosen_time
 
